e683d0f041017a9e606fbdd8d80edf97d6fab6e6
Umohmarvelous <umohmarvelous@gmail.com>
2025-10-25 23:32:43 +0100

    updated Lint errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,25 @@
 "use client"
 
 import { useRef } from "react"
-import { useScroll } from "framer-motion"
+// import { useScroll } from "framer-motion"
 import Navigation from "@/components/navigation"
 import Footer from "@/components/footer"
-import HeroSection from "@/components/sections/hero"
-import AboutSection from "@/components/sections/about"
-import ValuesSection from "@/components/sections/values"
-import TeamSection from "@/components/sections/team"
-import CustomersSection from "@/components/sections/customers"
+// import HeroSection from "@/components/sections/hero"
+// import AboutSection from "@/components/sections/about"
+// import ValuesSection from "@/components/sections/values"
+// import TeamSection from "@/components/sections/team"
+// import CustomersSection from "@/components/sections/customers"
 import HomePage from "./home-page/page"
 
 export default function Home() {
   const containerRef = useRef(null)
-  const { scrollYProgress } = useScroll({ container: containerRef })
+  // const { scrollYProgress } = useScroll({ container: containerRef })
 
   return (
     <div ref={containerRef} className="overflow-y-scroll h-screen bg-white">
       <Navigation />
-      <HomePage/>
-      {/* <HeroSection /> 
-      <AboutSection />
-      <CustomersSection />
-      <ValuesSection />
-      <TeamSection /> */}
+      <HomePage />
+
       <Footer /> 
     </div>
   )
