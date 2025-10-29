9d81642ad74c88e98cc58cb0d4b73d8bfbf357ff
Umohmarvelous <umohmarvelous@gmail.com>
2025-10-29 17:21:19 +0100

    update navigation.tsx

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -162,7 +162,7 @@ export default function Navigation() {
 
         {/* Mobile Navigation */}
         {isOpen && (
-          <div className="md:hidden p-5 pr-0 pt-5 top-0 left-0 right-0 absolute bg-white">
+          <div className=" md:hidden p-5 pr-0 pt-5 pb-16 top-0 left-0 right-0 absolute bg-white">
             {/* Mobile Menu Button */}
           <div className="flex flex-row w-full justify-between z-50">
             <Link href="/" className="text-xl font-bold text-white">
@@ -201,14 +201,14 @@ export default function Navigation() {
 
               <button
                 onClick={() => setIsMoreOpen(!isMoreOpen)}
-                className="px-4 py-2 text-left text-gray-500 hover:bg-gray-100 rounded-lg transition-colors text-sm font-medium flex items-center justify-between"
+                className="px-4 py-2 text-left text-gray-500 hover:bg-gray-100 rounded-lg transition-colors text-sm font-medium flex items-center justify-between w-full"
               >
                 More
                 <ChevronDown className={`w-4 h-4 transition-transform ${isMoreOpen ? "rotate-180" : ""}`} />
               </button>
 
               {isMoreOpen && (
-                <div className="py-3 px-4 bg-gray-300 rounded-xl flex items-center self-center justify-center flex-col text-center">
+                <div className="w-full py-3 px-4 bg-gray-300 rounded-xl flex items-center self-center justify-center flex-col text-center">
                   {moreLinks.map((link: NavLink) => (
                     <Link
                       key={link.href}
@@ -227,9 +227,6 @@ export default function Navigation() {
 
 
 
-              <button className=" px-4 py-2 text-left text-gray-500 hover:bg-gray-100 rounded-lg transition-colors text-sm font-medium">
-                <ShoppingCart className="w-5 h-5 inline-block mr-2" />
-              </button>
             </div>
           </div>
         )}
